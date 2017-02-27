12c78e4487b3e5acf8cae842e8df072864cc3d10
DISKOPierre <pierre.b@disko.fr>
2017-02-27 11:57:27 +0100

    Add The Deck Entity

---
+++ src/AppBundle/Entity/Deck.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Deck
+ */
+class Deck
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $number;
+
+    /**
+     * @ORM\OneToMany(targetEntity="eleusis\AppBundle\Entity\Card", mappedBy="deck")
+     */
+    private $cards;
+
+
+    public function __construct()
+    {
+        $this->cards = new ArrayCollection();
+        // ...
+    }
+
+    /**
+     * @param Card $card
+     * @return $this
+     */
+    public function addCard(Card $card)
+    {
+        $this->cards[] = $card;
+
+        return $this;
+    }
+
+    /**
+     * @param Card $card
+     */
+    public function removeCard(Card $card)
+    {
+        $this->cards->removeElement($card);
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getCards()
+    {
+        return $this->cards;
+    }
+    
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     *
+     * @return Deck
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return int
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
+
